9bb23bc929d9ce0ba8a2943e6e5f1be81136f07a
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-09-13 22:04:03 +0900

    Update posenet_sub_pub.py

--- posenet_pkg/posenet_sub_pub.py
+++ posenet_pkg/posenet_sub_pub.py
@@ -1,70 +1,44 @@
-import sys
-sys.path.append("/home/ydh/posenet_pkg/posenet_pkg/posenet_pkg")
 import rclpy
-from rclpy.node import Node 
-from sensor_msgs.msg import Image as Msg_img
-from geometry_msgs.msg import Pose
-from PIL import Image 
 import cv2
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
-import torch
-from data_loader import get_loader
-from solver import Solver
-from torch.backends import cudnn 
 
-class PoseNetSub_Pub(Node):
+class Image_pub(Node):
     def __init__(self):
-        super().__init__("posenet_sub_pub")
-        self.subscriber = self.create_subscription(Msg_img,'posenet',self.sub_callback,10)
-        self.publisher = self.create_publisher(Pose,'sixdof',10)   
+        super().__init__('image_pub')
+        qos_profile = QoSProfile(depth=10)
+        self.image_pub=self.create_publisher(CompressedImage,'/CompressedImage',qos_profile)
         self.cv_bridge = CvBridge()
-        self.subscriber
+        #self.timer=self.create_timer(1,self.publish_image_msg)
 
-    def sub_callback(self,img):
-        global pos
-        global ori
-        global flag
-        flag=True
-        msg=self.cv_bridge.imgmsg_to_cv2(img)
-        color_coverted = cv2.cvtColor(msg, cv2.COLOR_BGR2RGB)
-        pil_image=Image.fromarray(color_coverted)
-        cudnn.benchmark = True
-        data_loader = get_loader(model='Resnet', image_path=pil_image ,mode='test', batch_size=1)
-        sol=Solver(data_loader)
-        pos,ori=sol.test()
-        self.get_logger().info("image subscribed")
-        print("pos",pos)
-        print("ori",ori)  
-
-    def publish_callback(self):
-        global flag
-        msg=Pose()
-        msg.position.x=float(pos[0])
-        msg.position.y=float(pos[1])
-        msg.position.z=float(pos[2])
-        msg.orientation.x=float(ori[0])
-        msg.orientation.y=float(ori[1])
-        msg.orientation.z=float(ori[2])
-        msg.orientation.w=0.0
-        self.publisher.publish(msg)
-        self.get_logger().info("6 Dof published")
-        flag=False
+    def publish_image_msg(self): 
+        msg=CompressedImage()
+        vid_data = cv2.VideoCapture('/dev/video0',cv2.CAP_V4L)
+        vid_data.set(3,640)
+        vid_data.set(4,480)
+        #while(vid_data.isOpened()):
+        ret,image=vid_data.read()            
+        msg=self.cv_bridge.cv2_to_compressed_imgmsg(image)
+        self.image_pub.publish(msg)
+        print("published")
+        vid_data.release()
 
 def main(args=None):
     rclpy.init(args=args)
-    node= PoseNetSub_Pub()
+    node=Image_pub()
     try:
-        node.get_logger().info("Waitting msg....")
-        while(True):
-            rclpy.spin_once(node)   
-            if flag==True:
-                node.publish_callback()
-    
+        while(1):
+            rclpy.spin_once(node)
+            node.publish_image_msg()
+
     except KeyboardInterrupt:
         node.get_logger().info('Keyboard Interrupt (SIGINT)')
+
     finally:
         node.destroy_node()
         rclpy.shutdown()
 
-if __name__=="__main__":
+if __name__=='__main__':
     main()
